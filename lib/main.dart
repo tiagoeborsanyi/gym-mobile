38cc78df187c02f4fed2ed7f23f0ff8b71cace68
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-28 18:36:51 -0300

    created appBar static of home

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,32 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         primaryColor: Color(0xFFFF4500),
         accentColor: Color(0xFFFF8300),
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        canvasColor: Color.fromRGBO(255, 255, 255, 1),
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Gym Workout'),
+          title: Text(
+            'Gym Workout',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actions: [
+            Container(
+              width: 50,
+              height: 50,
+              // color: Colors.amber,
+              margin: EdgeInsets.only(right: 20),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber,
+              ),
+              child: Text(''),
+            )
+          ],
         ),
         body: Center(
           child: ElevatedButton(
