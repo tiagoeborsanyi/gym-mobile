f8c42043cbaab974e88d51df6c3443e049427e38
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-02 15:31:41 -0300

    set item on tabScreen component

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatefulWidget {
-  // const DashboardScreen({Key? key}) : super(key: key);
-
   @override
   _DashboardScreenState createState() => _DashboardScreenState();
 }
@@ -10,239 +8,208 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Dashboard',
-          style: TextStyle(
-            color: Colors.grey.shade700,
-            fontWeight: FontWeight.w900,
-            fontSize: 25,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          Container(
-            width: 50,
-            height: 50,
-            // color: Colors.amber,
-            margin: EdgeInsets.only(right: 20),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.grey.shade400,
+    return ListView(
+      children: [
+        Column(
+          children: [
+            SizedBox(
+              height: 20,
             ),
-            child: IconButton(
-              icon: Icon(Icons.person),
-              tooltip: 'Profile',
-              onPressed: () {},
-            ),
-          )
-        ],
-      ),
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 100,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.grey.shade300,
-                      width: 2,
-                    ),
-                    top: BorderSide(
-                      color: Colors.grey.shade300,
-                      width: 2,
-                    ),
+            Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 2,
                   ),
-                ),
-                child: Center(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          width: 110,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '25',
-                                style: TextStyle(fontSize: 25.0),
-                              ),
-                              Text(
-                                'Workouts completed',
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          width: 110,
-                          decoration: BoxDecoration(
-                            border: Border(
-                              right: BorderSide(
-                                color: Colors.grey.shade300,
-                                width: 2,
-                              ),
-                              left: BorderSide(
-                                color: Colors.grey.shade300,
-                                width: 2,
-                              ),
-                            ),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                '103',
-                                style: TextStyle(fontSize: 25.0),
-                              ),
-                              Text(
-                                'Workouts completed',
-                                textAlign: TextAlign.center,
-                              )
-                            ],
-                          ),
-                        ),
-                        Container(
-                          width: 110,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '70Kg',
-                                style: TextStyle(fontSize: 25.0),
-                              ),
-                              Text(
-                                'Workouts completed',
-                                textAlign: TextAlign.center,
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                  top: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 2,
                   ),
                 ),
               ),
-              Container(
-                height: 100,
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.grey.shade300,
-                      width: 2,
-                    ),
-                  ),
-                ),
-                padding: EdgeInsets.only(left: 20, right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 85,
-                          width: 70,
-                          margin: EdgeInsets.only(right: 20),
-                          decoration: BoxDecoration(color: Colors.amberAccent),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text('22'),
-                              Text('MAY'),
-                            ],
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 110,
+                        child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              'Previous workout',
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
+                              '25',
+                              style: TextStyle(fontSize: 25.0),
                             ),
                             Text(
-                              'Quad & Deltoids',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '7 exercises completed',
-                              style: TextStyle(
-                                fontSize: 13,
-                              ),
+                              'Workouts completed',
+                              textAlign: TextAlign.center,
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                    Container(
-                      child: Icon(
-                        Icons.keyboard_arrow_right_outlined,
-                        size: 35,
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'MY WORKOUT',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w800,
-                              color: Colors.grey.shade600),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Show All',
-                            style: TextStyle(fontSize: 19),
+                      Container(
+                        width: 110,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            right: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 2,
+                            ),
+                            left: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 2,
+                            ),
                           ),
                         ),
-                      ],
-                    ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              '103',
+                              style: TextStyle(fontSize: 25.0),
+                            ),
+                            Text(
+                              'Workouts completed',
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 110,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '70Kg',
+                              style: TextStyle(fontSize: 25.0),
+                            ),
+                            Text(
+                              'Workouts completed',
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 2,
+                  ),
+                ),
+              ),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 85,
+                        width: 70,
+                        margin: EdgeInsets.only(right: 20),
+                        decoration: BoxDecoration(color: Colors.amberAccent),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('22'),
+                            Text('MAY'),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Previous workout',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            'Quad & Deltoids',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '7 exercises completed',
+                            style: TextStyle(
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                   Container(
-                    height: 200,
-                    width: double.infinity,
-                    margin: EdgeInsets.only(left: 20, right: 20),
-                    decoration: BoxDecoration(color: Colors.grey.shade300),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('data'),
-                      ],
+                    child: Icon(
+                      Icons.keyboard_arrow_right_outlined,
+                      size: 35,
                     ),
                   )
                 ],
-              )
-            ],
-          )
-        ],
-      ),
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'MY WORKOUT',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.grey.shade600),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Show All',
+                          style: TextStyle(fontSize: 19),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  decoration: BoxDecoration(color: Colors.grey.shade300),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('data'),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dashboard_screen.dart';
+import 'statistic_screen.dart';
+
 class TabScreen extends StatefulWidget {
   const TabScreen({Key? key}) : super(key: key);
 
@@ -9,37 +12,88 @@ class TabScreen extends StatefulWidget {
 
 class _TabScreenState extends State<TabScreen> {
   int _selectedScreenIndex = 0;
-  late List<dynamic> _screens;
+  late List<Map<String, dynamic>> _screens;
 
   @override
   void initState() {
     super.initState();
-    _screens = [];
+    _screens = [
+      {
+        'title': 'Dashboard',
+        'screen': DashboardScreen(),
+      },
+      {
+        'title': 'Statistics',
+        'screen': StatisticScreen(),
+      },
+      {
+        'title': 'List Exercises',
+        'screen': Center(child: Text('List Exercises')),
+      },
+      {
+        'title': 'Configurations',
+        'screen': Center(child: Text('Config screen')),
+      }
+    ];
+  }
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (int i) {},
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title'],
+          style: TextStyle(
+            color: Colors.grey.shade700,
+            fontWeight: FontWeight.w900,
+            fontSize: 25,
+          ),
+        ),
         backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          Container(
+            width: 50,
+            height: 50,
+            // color: Colors.amber,
+            margin: EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey.shade400,
+            ),
+            child: IconButton(
+              icon: Icon(Icons.person),
+              tooltip: 'Profile',
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+      body: _screens[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Colors.green.shade100,
         unselectedItemColor: Colors.grey.shade400,
-        selectedItemColor: Colors.grey.shade900,
-        currentIndex: 0,
+        selectedItemColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedScreenIndex,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.graphic_eq),
+            icon: Icon(Icons.bar_chart_outlined),
             label: 'Graphs',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.clear_all_outlined),
-            label: 'List exercises',
+            label: 'Exercises',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
